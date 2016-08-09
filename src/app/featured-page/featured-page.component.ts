565aa1932d732a1d5d958f3d8f20f8ce268595fb
Tracy Lee <tracy@wingman.events>
2016-08-09 14:05:49 -0700

    good to go

--- src/app/featured-page/featured-page.component.ts
+++ src/app/featured-page/featured-page.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-featured-page',
-  styleUrls: ['featured-page.component.css']
+  styleUrls: ['featured-page.component.css'],
   template: `
   <div class="container">
     <div class="row">
